95907768e977b2a95ddbd36bab341eab658bda5c
Vishal Matsagar <vishalmatsagar0007@gmail.com>
2025-01-17 19:07:09 +0530

    created basic component that i have to style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
 }
 
 export default App;
+function Card() {
+  const [step, setStep] = useState(1);
+  function handdlePrev() {
+    setStep(step - 1);
+  }
+  function handdleNext() {
+    setStep(step + 1);
+  }
+  return (
+    <div className="card">
+      <Eelements
+        step={step}
+        handdlePrev={handdlePrev}
+        handdleNext={handdleNext}
+      />
+    </div>
+  );
+}
+function Eelements(props) {
+  return (
+    <>
+      <div className="steps">
+        <span className={props.step === 1 ? "active" : ""}>1</span>
+        <span className={props.step === 2 ? "active" : ""}>2</span>
+        <span className={props.step === 3 ? "active" : ""}>3</span>
+      </div>
+      <div className="main">
+        <h1>step : {props.step}</h1>
+      </div>
+      <div className="btn">
+        <button onClick={props.handdlePrev}>Prev</button>
+        <button onClick={props.handdleNext}>Next</button>
+      </div>
+    </>
+  );
+}
